409bb9d755b4a41b85cc14cce2104fec84b2d9f6
Victor Rodrigues <victor.carvalho@isalab.com.br>
2024-02-25 03:12:53 -0300

    feat: :sparkles: Add new component Sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,24 @@
+//Styles
+import { Avatar } from "../Avatar";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/rotciv-ohlavrac.png" />
+        <strong>Victor Rodrigues</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
+
+export { Sidebar };
